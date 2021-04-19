4fde4d04850c9f01b89b3e302ddfea8e8fdcaad6
Enif Lee <enif.lee@gmail.com>
2021-04-19 20:31:43 +0900

    init project for IAP.NET

---
+++ README.md
@@ -0,0 +1,10 @@
+# IAP.NET
+> In App(Game) Purchase validation provider.
+
+### Road map
+- [ ] Google Play Store
+- [ ] Apple appstore
+- [ ] Galaxy Store
+- [ ] Steam
+- [ ] OneStore (Korea)
+
---
+++ src/IAP.NET/Models/PurchaseProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace IAP.NET.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum PurchaseProvider
+    {
+        /// <summary>
+        /// google play store
+        /// </summary>
+        [EnumMember(Value = "Google")]
+        Google,
+        
+        /// <summary>
+        /// Apple appstore.
+        /// </summary>
+        [EnumMember(Value = "Apple")]
+        Apple,
+        
+        /// <summary>
+        /// Steam store.
+        /// </summary>
+        [EnumMember(Value = "Steam")]
+        Steam
+    }
+}
